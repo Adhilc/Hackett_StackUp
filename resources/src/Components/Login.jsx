b2ecd9ee721b363c1c19a9fd4c6e0c588dc890bd
anjalikallingal <anjalikallingal007@gmail.com>
2023-11-14 16:04:19 +0530

    shabil

--- resources/src/Components/Login.jsx
+++ resources/src/Components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import image from "./brain.jpeg"
 import "./Login.css";
 import { useNavigate } from 'react-router-dom';
